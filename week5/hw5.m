971d4f0adae7246c94d86414ac8e416742c8d532
Norbert Scheller <nscheller@actian.com>
2014-05-02 13:36:39 +0200

    homework 5 finished.

--- week5/hw5.m
+++ week5/hw5.m
@@ -1,3 +1,5 @@
+clear;
+
 Po = imread('original.jpg');
 Pn = imread('noisy.jpg');
 
@@ -7,10 +9,15 @@ Io = double(Po);
 I1 = double(medfilt2(uint8(In)));
 I2 = double(medfilt2(uint8(I1)));
 
-psnr1 = PSNR(Io, In);
-psnr2 = PSNR(Io, I1);
-psnr3 = PSNR(Io, I2);
+psnr1 = PSNR(Io, In, 255);
+psnr2 = PSNR(Io, I1, 255);
+psnr3 = PSNR(Io, I2, 255);
 
 sprintf('PSNR(Io, In) = %f', psnr1)
 sprintf('PSNR(Io, I1) = %f', psnr2)
 sprintf('PSNR(Io, I2) = %f', psnr3)
+
+figure; imshow(uint8(In))
+figure; imshow(uint8(I1))
+figure; imshow(uint8(I2))
+figure; imshow(uint8(Io))
