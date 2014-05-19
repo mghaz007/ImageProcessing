2a81d40663c393463f5a9afa489e5d04ded37abc
Norbert Scheller <nscheller@actian.com>
2014-05-19 11:31:48 +0200

    week7 solved

---
+++ week7/wrapper.m
@@ -0,0 +1,31 @@
+clear all
+close all
+
+%% Simulate 1-D blur and noise
+image_original = im2double(imread('Cameraman256.bmp', 'bmp'));
+[H, W] = size(image_original);
+blur_impulse = fspecial('motion', 7, 0);
+image_blurred = imfilter(image_original, blur_impulse, 'conv', 'circular');
+noise_power = 1e-4;
+randn('seed', 1);
+noise = sqrt(noise_power) * randn(H, W);
+image_noisy = image_blurred + noise;
+
+figure; imshow(image_original, 'border', 'tight');
+figure; imshow(image_blurred, 'border', 'tight');
+figure; imshow(image_noisy, 'border', 'tight');
+
+%% CLS restoration
+alpha = 1;  % you should try different values of alpha
+values = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100];
+
+for alpha=values
+  image_cls_restored = cls_restoration(image_noisy, blur_impulse, alpha);
+
+%% computation of ISNR
+  isnr = 10 * log10((norm(image_original - image_noisy, 'fro') ^ 2) / ( norm(image_original - image_cls_restored, 'fro') ^ 2) );
+  sprintf('alpha = %f, ISNR = %f', alpha, isnr)
+  
+  lbl = strcat('ALPHA: ', num2str(alpha), ' ISNR:', num2str(isnr))
+  figure('Name', lbl); imshow(image_cls_restored, 'border', 'tight');
+end
