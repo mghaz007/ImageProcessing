43f74507e0b502d12b9f1813d83f757b5b04f449
Norbert Scheller <nscheller@actian.com>
2014-05-27 10:59:15 +0200

    PSNR calculated with norm

--- week9/PSNR.m
+++ week9/PSNR.m
@@ -1,6 +1,9 @@
 function g = PSNR(B1,B2,val)
 
-mse  = MSE(B1, B2);
-psnr = 10 * log10(val^2 / mse);
+[H,W] = size(B1);
+psnr = 10 * log10(val^2 / (norm(B1 - B2, 'fro') ^ 2 / H / W));
+
+% mse  = MSE(B1, B2);
+% psnr = 10 * log10(val^2 / mse);
 
 g = psnr;
